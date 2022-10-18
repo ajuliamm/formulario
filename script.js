afbb8bd474f3c39b93dbbce1336b49204bf08fc4
Angelina Julia <ajuliammelo@gmail.com>
2022-10-18 00:16:11 -0300

    js

--- script.js
+++ script.js
@@ -8,13 +8,33 @@ event.preventDefault()
 const validation = (event) =>{
    const field = event.target
 
-   //trocar mensagem de required
-   field.setCustomValidity('Esse campo é obrigatório')
+   
+//lógica para verificar se existem erros 
+   function verifyErros (){
+    let foundError = false
 
+    for(let error in field.validity){
+        //se não for customError então verifica se tem erro
+        if(error != "customError" && field.validity[error]){
+            foundError = error
+        }
+    return foundError
+    }
+
+   }
+   const error = verifyErros()
+   console.log(error)
+
+   //trocar mensagem de required
+   if(error){
+   field.setCustomValidity('Esse campo é obrigatório')
+   }else{
+    field.setCustomValidity('')
+   }
 }
 
 const fields = document.querySelectorAll('[required]')
-for(field of fields){
+for(let field of fields){
     /* invalid é um evento que é possível usar quando o elemento chamado é o required*/
     field.addEventListener('invalid', validation)
 }
